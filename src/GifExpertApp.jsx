284356ed89b84c044e6721ea9a2d810d311c2059
Ademir Rivera <uarivera@innovacion.gob.sv>
2025-05-12 13:40:04 -0600

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState
+
+    return (
+        <>
+            { /* titulo */}
+            <h1> GiftExpertApp </h1>
+
+            { /* input */}
+
+            { /* listado de gif */}
+
+            { /* gif item */}
+        </>
+    )
+}
