2134be884da320e85f14fd4ef3deadf1195f172a
Ademir Rivera <themrademir@gmail.com>
2025-05-19 09:02:09 -0600

    feat: integrando peticion a la api para obtener datos de prueba

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,30 +1,24 @@
 import { useState } from "react"
 import { AddCategory } from './components/AddCategory'
+import { GifGrid } from "./components/GifGrid"
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+    const [categories, setCategories] = useState(['One Punch'])
 
     const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return
+
         setCategories([newCategory, ...categories])
     }
 
     return (
         <>
-            { /* titulo */}
             <h1> GiftExpertApp </h1>
 
-            { /* input */}
             <AddCategory onNewCategory={onAddCategory} />
 
-            { /* listado de gif */}
-            <ol>
-                {categories.map(category => {
-                    return <li key={category}> {category} </li>
-                })}
-            </ol>
-
-            { /* gif item */}
+            {categories.map((category) => (<GifGrid key={category} category={category} />))}
         </>
     )
 }
